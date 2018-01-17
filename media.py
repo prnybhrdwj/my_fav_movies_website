e5b19d37a7324868203cd0efcd2427d1f01a1f89
Pranay Bhardwaj <prnybhrdwj@gmail.com>
2018-01-17 22:18:00 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,17 +1,22 @@
 import webbrowser
- 
-#This document will have all the constructors listed
+
+# This document will have all the constructors listed
+
 
 class Media():
-    '''We are defining a constructor of Media so that we can reuse it for tv series, documentaries, etc as well as movies'''
+    '''We are defining a constructor of Media so that we
+        can reuse it for tv series, documentaries, etc as well as movies'''
     def __init__(self, media_type):
         self.media_type = media_type
 
-#We will define child class movie here.
-        
+# We will define child class movie here.
+
+
 class Movie(Media):
     '''We are defining a child class of Media - Movie.'''
-    def __init__(self, media_type, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(
+            self, media_type, movie_title, movie_storyline,
+            poster_image, trailer_youtube):
         Media.__init__(self, media_type)
         self.title = movie_title
         self.storyline = movie_storyline
@@ -20,4 +25,3 @@ class Movie(Media):
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-
